fc8f86047225ede16ecc799ef9a3d576ce48807b
Wangyisu520 <43199213+Wangyisu520@users.noreply.github.com>
2020-02-09 16:17:27 +0800

    react
    react-koa

---
+++ build/precache-manifest.1fab68ba19275747def46e2bd4073fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e291e79c415a672d578caa23a2de0f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c122ce48abbda62d36f0",
+    "url": "/static/css/main.db2c17b7.chunk.css"
+  },
+  {
+    "revision": "5eee1e8c7c06f9ad54e3",
+    "url": "/static/js/2.06ef0ee1.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.06ef0ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c122ce48abbda62d36f0",
+    "url": "/static/js/main.06099f53.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
